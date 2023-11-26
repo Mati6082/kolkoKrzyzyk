3fe4ec85f2959131a61991ab82b9616be0d1e3a4
matpakla <matpakla@gmail.com>
2023-11-26 21:20:53 +0100

    dziala

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
 
 using namespace std;
 
@@ -7,58 +8,61 @@ class Gra {
 public:
     vector<int> plansza{0, 0, 0, 0, 0, 0, 0, 0, 0};
     bool winner;
+    int wart = -1;
 
     void wyswietlNrPol() {
         cout << "Numery pol:\n";
         for (int i = 0; i < plansza.size(); i++) {
             cout << i+1 << " ";
-            if (i % 3 == 0 && i != 0) cout << endl;
+            if ((i+1) % 3 == 0 && i != 0) cout << endl;
         }
-        cout << endl;
     }
 
     void wyswietl() {
         cout << "Plansza:\n";
         for (int i = 0; i < plansza.size(); i++) {
             if (i % 3 == 0 && i != 0) cout << endl;
-            if (plansza[i+1] == 1) cout << "X ";
-            else if (plansza[i+1] == -1) cout << "O ";
+            if (plansza[i] == 1) cout << "X ";
+            else if (plansza[i] == -1) cout << "O ";
             else cout << "- ";
-        }
-    }
-
-    int sprawdzRzad() {
-        for (int i = 0; i < 9; i += 3) {
-            if (plansza[i] + plansza[i + 1] + plansza[i + 2] == 3) return 3;
-            if (plansza[i] + plansza[i + 1] + plansza[i + 2] == -3) return -3;
-        }
-        return 0;
-    }
-
-    int sprawdzKol() {
-        for (int i = 0; i < 3; i++) {
-            if (plansza[i] + plansza[i + 3] + plansza[i + 6] == 3) return 3;
-            if (plansza[i] + plansza[i + 3] + plansza[i + 6] == -3) return -3;
-        }
-        return 0;
-    }
-
-    int sprawdzPrzek() {
-        if (plansza[0] + plansza[4] + plansza[8] == 3 ||
-            plansza[2] + plansza[4] + plansza[6] == 3)
-            return 3;
-        if (plansza[0] + plansza[4] + plansza[8] == -3 ||
-            plansza[2] + plansza[4] + plansza[6] == -3)
-            return -3;
-        return 0;
+        } cout << endl;
     }
 
     bool sprawdz() {
-        if (sprawdzKol() == 3 || sprawdzRzad() == 3 || sprawdzPrzek() == 3) {
+        int suma;
+        for (int i = 0; i < 3; i++) {
+            suma = plansza[i] + plansza[i + 3] + plansza[i + 6];
+            if (suma == 3) {
+                winner = true;
+                return true;
+            } else if (suma == -3) {
+                winner = false;
+                return true;
+            }
+        }
+        for (int i = 0; i < 9; i+=3) {
+            suma = plansza[i] + plansza[i + 1] + plansza[i + 2];
+            if (suma == 3) {
+                winner = true;
+                return true;
+            } else if (suma == -3) {
+                winner = false;
+                return true;
+            }
+        }
+        suma = plansza[0] + plansza[4] + plansza[8];
+        if (suma == 3) {
             winner = true;
             return true;
+        } else if (suma == -3) {
+            winner = false;
+            return true;
         }
-        if (sprawdzKol() == -3 || sprawdzRzad() == -3 || sprawdzPrzek() == -3) {
+        suma = plansza[2] + plansza[4] + plansza[6];
+        if (suma == 3) {
+            winner = true;
+            return true;
+        } else if (suma == -3) {
             winner = false;
             return true;
         }
@@ -75,6 +79,14 @@ public:
     bool end() {
         return sprawdz() || czyPusta();
     }
+
+    void ruch() {
+        int ruchBota = rand() % 10;
+        while (plansza[ruchBota] != 0) {
+            ruchBota = rand() % 10;
+        }
+        plansza[ruchBota] = wart;
+    }
 };
 
 class Gracz {
@@ -88,11 +100,13 @@ public:
         game.wyswietl();
         cout << "Podaj pole: ";
         cin >> pole;
-        while (game.plansza[pole-1] != 0) {
+        pole--;
+        while (game.plansza[pole] != 0 || pole > 8) {
             cout << "Podaj pole: ";
             cin >> pole;
+            pole--;
         }
-        game.plansza[pole-1] = wart;
+        game.plansza[pole] = wart;
     }
 
     Gracz(Gra game) {
@@ -101,12 +115,19 @@ public:
 };
 
 int main() {
+    srand(time(nullptr));
     Gra game;
     Gracz player(game);
     while(!game.end()) {
         player.ruch();
         game.plansza = player.game.plansza;
+        if (game.end()) break;
+        game.ruch();
+        player.game.plansza = game.plansza;
     }
-    if (game.winner) cout << "W";
+    game.wyswietl();
+    if (game.winner) cout << "WINNER";
+    else if (!game.winner) cout << "LOSER";
+    else cout << "DRAW";
     return 0;
 }
